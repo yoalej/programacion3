18ee000340c01803377d936e01bea013186e662c
ale <aleyo7835@gmail.com>
2025-09-09 20:19:48 -0600

    mi primer entorno virtual

---
+++ EntornoVir.py
@@ -0,0 +1,19 @@
+# Usando pip list
+
+#Este es el comando más común y fácil de leer para ver los paquetes instalados.
+#  Muestra una tabla con el nombre del paquete y su versión.
+
+
+#pip list
+#Si quieres ver los paquetes que tienen actualizaciones disponibles, puedes usar:
+
+
+#pip list --outdated
+#Esto mostrará una tabla con los paquetes que no están en su última versión,
+
+#pip install requests==
+#Esto fallará, pero en el mensaje de error mostrará una lista de todas
+#las versiones de requests que puedes instalar. Es un truco muy útil.
+
+#pip install requests==2.25.1
+#Esto instalará la versión 2.26.1 de requests.
